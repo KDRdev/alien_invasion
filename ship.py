e503ed207b45107fdb6250a80be22ed064b15c84
dkdev <dmitry.kashuba.cv@gmail.com>
2020-07-31 14:52:02 +0200

    Added ship_speed, screen limits for ship movement

--- ship.py
+++ ship.py
@@ -8,6 +8,8 @@ class Ship:
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
+        self.settings = ai_game.settings
+
         # Load a ship image and get its rect.
         self.image = pygame.image.load("images/ship.bmp")
         self.rect = self.image.get_rect()
@@ -16,14 +18,18 @@ class Ship:
         self.moving_left = False
         self.moving_right = False
 
-        # Position the ship rect at the middle bottom part of the screen.
+        # Position the ship rect at the middle bottom part of the screen and 
+        # store its horizontal position as a decimal.
         self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
 
     def update(self):
-        if self.moving_left:
-            self.rect.x -= 1
-        if self.moving_right:
-            self.rect.x += 1
+        # Moves the ship by changing its horizontal position by ship_speed
+        if self.moving_left and self.x >= 0:
+            self.x -= self.settings.ship_speed
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        self.rect.x = self.x
 
     def blitme(self):
         # Draw the ship at its current position.
