a91dc930cef8c63f1c6d75887954e1e3436b5429
dkdev <dmitry.kashuba.cv@gmail.com>
2020-07-31 07:40:55 +0200

    Added moving left/right functionality for the ship

--- ai.py
+++ ai.py
@@ -23,7 +23,18 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+                    elif event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
+                    elif event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
             self.screen.fill(self.bg_color)
+            self.ship.update()
             self.ship.blitme()
             pygame.display.flip()
 
--- ship.py
+++ ship.py
@@ -12,8 +12,18 @@ class Ship:
         self.image = pygame.image.load("images/ship.bmp")
         self.rect = self.image.get_rect()
 
+        # A flag for moving the ship
+        self.moving_left = False
+        self.moving_right = False
+
         # Position the ship rect at the middle bottom part of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+
+    def update(self):
+        if self.moving_left:
+            self.rect.x -= 1
+        elif self.moving_right:
+            self.rect.x += 1
 
     def blitme(self):
         # Draw the ship at its current position.
